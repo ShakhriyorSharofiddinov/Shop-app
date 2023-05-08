b5210edf2a40132d71ac2f8c29e2189143bc44ec
Shakhriyor <shaxriyorsharofiddinov@gmail.com>
2023-05-08 06:07:11 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/screens/home/home_screen.dart';
+
+import 'constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Shop App',
+      theme: ThemeData(
+        scaffoldBackgroundColor: bgColor,
+        primarySwatch: Colors.blue,
+        fontFamily: "Gordita",
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        textTheme: const TextTheme(
+          bodyText2: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
